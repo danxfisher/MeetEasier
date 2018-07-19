0e6ef6c931a91bd19db96fefe9be0398eff25410
r1bb3t <r1bb3t@gmail.com>
2018-07-18 18:36:44 -0700

    remove front end handling of room-blacklist

--- ui-react/src/components/flightboard/FlightboardRow.js
+++ ui-react/src/components/flightboard/FlightboardRow.js
@@ -8,8 +8,7 @@ class FlightboardRow extends Component {
     super(props);
     this.state = {
       nextUp: '',
-      timesPresent: false,
-      skippedRoom: false
+      timesPresent: false
     }
   }
 
@@ -32,25 +31,12 @@ class FlightboardRow extends Component {
     }
   }
 
-  isRoomSkipped = () => {
-    const { item } = this.props;
-
-    fbConfig.board.roomsToSkip.forEach(function(configItem, configKey) {
-      if(configItem === item.Email) {
-        this.setState({
-          skippedRoom: true
-        });
-      }
-    });
-  }
-
   componentDidMount = () => {
-    this.isRoomSkipped();
     this.getAppointmentTime();
   }
 
   render() {
-    const { nextUp, skippedRoom, timesPresent } = this.state;
+    const { nextUp, timesPresent } = this.state;
     const { item, now } = this.props;
 
     const styles = {
@@ -62,79 +48,77 @@ class FlightboardRow extends Component {
     const room = item.Name.toLowerCase().replace(/\s+/g, "-");
     const roomlist = 'roomlist-' + item.Roomlist.toLowerCase().replace(/\s+/g, "-");
 
-    // if the room needs to be skipped, return null
-    if (!skippedRoom) {
-      let meetingRoomClass = `${ room } meeting-room ${ item.Busy ? 'meeting-room-busy' : '' }`;
-      meetingRoomClass += item.Busy ? ' meeting-room-busy' : '';
-      meetingRoomClass += item.ErrorMessage ? ' meeting-room-error' : '';
-      const meetingClass = item.ErrorMessage
-        ? 'meeting-error'
-        : item.Busy
-          ? 'meeting-busy'
-          : 'meeting-open';
-      let statusText = item.ErrorMessage
-        ? fbConfig.board.text.statusError
-        : item.Busy
-          ? fbConfig.board.text.statusBusy
-          : fbConfig.board.text.statusAvailable;
+    // set row class based on meet room status
+    let meetingRoomClass = `${ room } meeting-room ${ item.Busy ? 'meeting-room-busy' : '' }`;
+    meetingRoomClass += item.Busy ? ' meeting-room-busy' : '';
+    meetingRoomClass += item.ErrorMessage ? ' meeting-room-error' : '';
+    const meetingClass = item.ErrorMessage
+      ? 'meeting-error'
+      : item.Busy
+        ? 'meeting-busy'
+        : 'meeting-open';
+    let statusText = item.ErrorMessage
+      ? fbConfig.board.text.statusError
+      : item.Busy
+        ? fbConfig.board.text.statusBusy
+        : fbConfig.board.text.statusAvailable;
 
-      return (
-        <div className={'row-padder ' + roomlist} style={this.props.filter === roomlist || this.props.filter === 'roomlist-all' || this.props.filter === '' ? styles.show : styles.hide}>
-          <div className="row">
-            <div className="medium-12 columns">
-              <div className={meetingRoomClass}>
-                <div className="row valign-middle">
-                  <div className={room + '-status meeting-room__status medium-2 columns'}>
-                    <div className={meetingClass} title={item.ErrorMessage || ''}>
-                      {statusText}
-                    </div>
+    return (
+      <div className={'row-padder ' + roomlist} style={this.props.filter === roomlist || this.props.filter === 'roomlist-all' || this.props.filter === '' ? styles.show : styles.hide}>
+        <div className="row">
+          <div className="medium-12 columns">
+            <div className={meetingRoomClass}>
+              <div className="row valign-middle">
+                <div className={room + '-status meeting-room__status medium-2 columns'}>
+                  <div className={meetingClass} title={item.ErrorMessage || ''}>
+                    {statusText}
                   </div>
-                  <div className="medium-3 columns">
-                    <div className={room + '-name meeting-room__name'}>
-                      {item.Name}
-                    </div>
+                </div>
+                <div className="medium-3 columns">
+                  <div className={room + '-name meeting-room__name'}>
+                    {item.Name}
                   </div>
-                  <div className="medium-6 columns">
-                    <div className={room + '-meeting-information'}>
-                      {timesPresent && item.Appointments[0].End >= now &&
-                        <div>
-                          <span className={room + '-meeting-upcoming meeting-upcoming'}>
-                            {nextUp}
-                          </span>
-                          <span className={room + '-subject meeting-subject'}>
-                            {item.Appointments[0].Subject}
-                          </span>
-                        </div>
-                      }
-                    </div>
-                    <div className={room + '-time meeting-time'}>
-                      {timesPresent ?
-                        new Date(parseInt(item.Appointments[0].Start, 10)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) + ' - ' + new Date(parseInt(item.Appointments[0].End, 10)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
-                        : ''
-                      }
-                    </div>
-                    <div className={room + '-organizer meeting-organizer'}>
-                      {timesPresent && item.Appointments[0].End >= now &&
-                        item.Appointments[0].Organizer
-                      }
-                    </div>
+                </div>
+                <div className="medium-6 columns">
+                  <div className={room + '-meeting-information'}>
+                    {timesPresent && item.Appointments[0].End >= now &&
+                      <div>
+                        <span className={room + '-meeting-upcoming meeting-upcoming'}>
+                          {nextUp}
+                        </span>
+                        <span className={room + '-subject meeting-subject'}>
+                          {item.Appointments[0].Subject}
+                        </span>
+                      </div>
+                    }
                   </div>
-                  <div className="medium-1 columns">
-                    <div className="meeting-fullscreen">
+                  <div className={room + '-time meeting-time'}>
+                    {timesPresent ?
+                      new Date(parseInt(item.Appointments[0].Start, 10)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) + ' - ' + new Date(parseInt(item.Appointments[0].End, 10)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+                      : ''
+                    }
+                  </div>
+                  <div className={room + '-organizer meeting-organizer'}>
+                    {timesPresent && item.Appointments[0].End >= now &&
+                      item.Appointments[0].Organizer
+                    }
+                  </div>
+                </div>
+                <div className="medium-1 columns">
+                  <div className="meeting-fullscreen">
 
-                      <Link to={'/single-room/' + room} target="_blank">
-                        <i className="fi-monitor"></i>
-                      </Link>
+                    <Link to={'/single-room/' + room} target="_blank">
+                      <i className="fi-monitor"></i>
+                    </Link>
 
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      )
-    }
+      </div>
+    )
   }
 }
 
