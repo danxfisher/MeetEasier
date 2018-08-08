433e3ca2b18dd9411b089505088784abc1c3fd37
r1bb3t <r1bb3t@gmail.com>
2018-08-08 00:50:53 -0700

    added singleroomlayout unit tests

--- ui-react/src/layouts/SingleRoomLayout.test.js
+++ ui-react/src/layouts/SingleRoomLayout.test.js
@@ -1,6 +1,38 @@
+import SingleRoomLayout from './SingleRoomLayout';
+
 
 describe('SingleRoomLayout Component', () => {
-  it('renders correctly', () => {
+  let props, wrapper;;
+  beforeEach(() => {
+    props = {
+      match: {
+        params: {
+          name: 'test-room'
+        }
+      }
+    };
 
+    wrapper = shallow(<SingleRoomLayout {...props} />);
+  });
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders Display component if roomAlias exists', () => {
+    const component = wrapper.find('Display').exists();
+    expect(component).toBeTruthy();
+  });
+
+  it('renders Display component props correctly', () => {
+    const alias = wrapper.find('Display').props().alias;
+    expect(alias).toEqual(props.match.params.name);
+  })
+
+  it('renders NotFound if roomAlias does not exists', () => {
+    wrapper.setState({ roomAlias: '' });
+
+    const component = wrapper.find('NotFound').exists();
+    expect(component).toBeTruthy();
   });
 });
