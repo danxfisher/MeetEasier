74ed5e75de0568825c612f3e51c74353110bbb26
Collie147 <Colin.russellconway@gmail.com>
2019-08-09 16:16:06 +0100

    try catch for room.appointments.length
    room.appointments.length my not be loaded in time.  Try-Catch typeerror reloads the page if it catches this error.

--- ui-react/src/components/single-room/RoomStatusBlock.js
+++ ui-react/src/components/single-room/RoomStatusBlock.js
@@ -66,15 +66,72 @@ function ButtonControl(props){
   let room = props.room;
   let details = props.details;
   var moment = require('moment');
-  if (room.Busy){
-    let currentAppointmentEnd = new Date(parseInt(room.Appointments[0].End, 10));
-    if (room.Appointments.length > 1){
-      let nextAppointmentStart = new Date(parseInt(room.Appointments[1].Start, 10));
-      
-      currentAppointmentEnd = moment(currentAppointmentEnd);
-      nextAppointmentStart = moment(nextAppointmentStart);
-      let timeDifference = nextAppointmentStart.diff(currentAppointmentEnd, 'minutes')
-      if (timeDifference > 120){
+  try{
+    if (room.Busy){
+      let currentAppointmentEnd = new Date(parseInt(room.Appointments[0].End, 10));
+      if (room.Appointments.length > 1){
+        let nextAppointmentStart = new Date(parseInt(room.Appointments[1].Start, 10));
+        
+        currentAppointmentEnd = moment(currentAppointmentEnd);
+        nextAppointmentStart = moment(nextAppointmentStart);
+        let timeDifference = nextAppointmentStart.diff(currentAppointmentEnd, 'minutes')
+        if (timeDifference > 120){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("60", room, currentAppointmentEnd)}} id="bookAfterButton1h">1 Hour</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("120", room, currentAppointmentEnd)}} id="bookAfterButton2h">2 Hours</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 60){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("60", room, currentAppointmentEnd)}} id="bookAfterButton1h">1 Hour</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 45){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("45", room, currentAppointmentEnd)}} id="bookAfterButton45">45 Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 30){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 15){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookAfter(timeDifference, room, currentAppointmentEnd)}} id="bookAfterButtonTD">{timeDifference} Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference >= 5){
+          return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookAfter(timeDifference, room, currentAppointmentEnd)}} id="bookAfterButtonTD">{timeDifference} Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else {
+          return (<div> <h4> Room is unavailable </h4></div>);
+        }
+      }
+      else
+      {
         return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
           <tr>
             <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
@@ -84,74 +141,74 @@ function ButtonControl(props){
           </tr></table></div>
         );
       }
-      else if (timeDifference > 60){
-        return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("60", room, currentAppointmentEnd)}} id="bookAfterButton1h">1 Hour</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 45){
-        return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("45", room, currentAppointmentEnd)}} id="bookAfterButton45">45 Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 30){
-        return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 15){
-        return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookAfter(timeDifference, room, currentAppointmentEnd)}} id="bookAfterButtonTD">{timeDifference} Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference >= 5){
-        return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookAfter(timeDifference, room, currentAppointmentEnd)}} id="bookAfterButtonTD">{timeDifference} Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else {
-        return (<div> <h4> Room is unavailable </h4></div>);
-      }
     }
-    else
-    {
-      return (<div> <h4> Extend/Book after this meeting Now: </h4><table width='100%'>
-        <tr>
-          <td><button type="button" class="btn" onClick={e => {BookAfter("15", room, currentAppointmentEnd)}} id="bookAfterButton15">15 Minutes</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookAfter("30", room, currentAppointmentEnd)}} id="bookAfterButton30">30 Minutes</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookAfter("60", room, currentAppointmentEnd)}} id="bookAfterButton1h">1 Hour</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookAfter("120", room, currentAppointmentEnd)}} id="bookAfterButton2h">2 Hours</button></td>
-        </tr></table></div>
-      );
-    }
-  }
-  else {
-    if (room.Appointments.length > 0){
-      let now = new Date();
-      console.log("DATETIMENOW = " + now);
-      let nextAppointmentStart = new Date(parseInt(room.Appointments[0].Start, 10));
-      now = moment(now);
-      console.log("DATETIMENOW2 = " + now);
-      nextAppointmentStart = moment(nextAppointmentStart);
+    else {
+      if (room.Appointments.length > 0){
+        let now = new Date();
+        console.log("DATETIMENOW = " + now);
+        let nextAppointmentStart = new Date(parseInt(room.Appointments[0].Start, 10));
+        now = moment(now);
+        console.log("DATETIMENOW2 = " + now);
+        nextAppointmentStart = moment(nextAppointmentStart);
 
-      let timeDifference = nextAppointmentStart.diff(now, 'minutes')
-      if (timeDifference > 120){
+        let timeDifference = nextAppointmentStart.diff(now, 'minutes')
+        if (timeDifference > 120){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("60", room)}} id="bookNowButton1h">1 Hour</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("120", room)}} id="bookNowButton2h">2 Hours</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 60){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}}>15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("60", room)}} id="bookNowButton1h">1 Hour</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 45){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("45", room)}} id="bookNowButton45">45 Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 30){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference > 15){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
+              <td><button type="button" class="btn" onClick={e => {BookNow(timeDifference, room)}} id="bookNowButtonTD">{timeDifference} Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else if (timeDifference >= 5){
+          return (<div> <h4> Book This Room Now: </h4><table width='100%'>
+            <tr>
+              <td><button type="button" class="btn" onClick={e => {BookNow(timeDifference, room)}} id="bookNowButtonTD">{timeDifference} Minutes</button></td>
+            </tr></table></div>
+          );
+        }
+        else {
+          return (<div> <h4> Room is unavailable before next booking.</h4></div>);
+        }
+      }
+      else
+      {
         return (<div> <h4> Book This Room Now: </h4><table width='100%'>
           <tr>
             <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
@@ -161,62 +218,10 @@ function ButtonControl(props){
           </tr></table></div>
         );
       }
-      else if (timeDifference > 60){
-        return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}}>15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow("60", room)}} id="bookNowButton1h">1 Hour</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 45){
-        return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow("45", room)}} id="bookNowButton45">45 Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 30){
-        return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference > 15){
-        return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
-            <td><button type="button" class="btn" onClick={e => {BookNow(timeDifference, room)}} id="bookNowButtonTD">{timeDifference} Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else if (timeDifference >= 5){
-        return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-          <tr>
-            <td><button type="button" class="btn" onClick={e => {BookNow(timeDifference, room)}} id="bookNowButtonTD">{timeDifference} Minutes</button></td>
-          </tr></table></div>
-        );
-      }
-      else {
-        return (<div> <h4> Room is unavailable before next booking.</h4></div>);
-      }
     }
-    else
-    {
-      return (<div> <h4> Book This Room Now: </h4><table width='100%'>
-        <tr>
-          <td><button type="button" class="btn" onClick={e => {BookNow("15", room)}} id="bookNowButton15">15 Minutes</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookNow("30", room)}} id="bookNowButton30">30 Minutes</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookNow("60", room)}} id="bookNowButton1h">1 Hour</button></td>
-          <td><button type="button" class="btn" onClick={e => {BookNow("120", room)}} id="bookNowButton2h">2 Hours</button></td>
-        </tr></table></div>
-      );
-    }
+  }
+  catch (TypeError){
+    setTimeout(reloadPage, 3000);
   }
 }
 const RoomStatusBlock = ({ config, details, room }) => (
