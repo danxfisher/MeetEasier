59671f6404c2e14a380c5401a755fe857e2cc856
Collie147 <Colin.russellconway@gmail.com>
2019-08-09 13:18:54 +0100

    Added listen for book now/extend buttons

--- app/routes.js
+++ app/routes.js
@@ -43,10 +43,25 @@ module.exports = function(app) {
 	});
 
 	// heartbeat-service to check if server is alive
-  app.get('/api/heartbeat', function(req, res) {
-    res.json({ status: 'OK' });
-  });
+ 	app.get('/api/heartbeat', function(req, res) {
+    	res.json({ status: 'OK' });
+  	});
+	
+	// books a room
+	app.get('/api/roombooking', function(req, res){
 
+		console.log("Route Room Booking");
+		//console.log(req);
+
+		var ews = require('./ews/roombooking.js');
+		var roomEmail = req.query.roomEmail;
+		var roomName = req.query.roomName;
+		var startTime = req.query.startTime;
+		var endTime = req.query.endTime;
+		console.log(roomEmail+" | "+roomName+" | "+startTime+" | "+endTime);
+		ews.BookRoom(roomEmail, roomName, startTime, endTime);
+			res.json({ status: 'Booked' });
+	});
 	// redirects everything else to our react app
 	app.get('*', function(req, res) {
 		res.sendFile(path.join(__dirname,'../ui-react/build/','index.html'));
